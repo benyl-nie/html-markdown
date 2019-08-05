b1a9123548cee79c5abdb5bfc5fb430b884a0bb7
nieyuling02 <nieyuling02@maoyan.com>
2019-08-05 20:02:07 +0800

    html2md手写

---
+++ server/html2md.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+const demostr = require('./tpl/ftl');
+
+const getHtmlByUrl = async (href) => {
+  return new Promise((resolve, reject) => {
+    request(href, (err, response, body) => {
+      if (!err && response.statusCode == 200) {
+        // console.log(body); // body即为目标页面的html
+        html2Md(body);
+        resolve();
+      } else {
+        console.log('get page error url => ' + href);
+        reject();
+      }
+    });
+  });
+}
+
+const html2Md = async (str) => {
+  return new Promise((resolve, reject) => {
+    let $ = cheerio.load(str);
+    let contentStr = $('#main-content').html();
+  });
+}
+
+// module.exports = getHtmlByUrl('https://www.baidu.com/');
+module.exports = html2Md(demostr);
