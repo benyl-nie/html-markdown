2c80a7fda05a6c304e997c36388fb8dddf6b0497
nieyuling02 <nieyuling02@maoyan.com>
2019-08-06 16:09:35 +0800

    字符转markdown

--- server/html2md.js
+++ server/html2md.js
@@ -1,18 +1,29 @@
 const request = require('request');
 const cheerio = require('cheerio');
+// const lodash = require('lodash');
+// const xpath = require('xpath.js');
+const xmlDom = require('xmldom').DOMParser;
 
 const demostr = require('./tpl/ftl');
+
+let markdownStr = ``;
+
+const tagHtml = [
+  {key: 'h1', value: '#'},
+  {key: 'h2', value: '##'},
+  {key: 'h3', value: '###'},
+  {key: 'h4', value: '####'},
+  {key: 'h5', value: '#####'},
+  {key: 'h6', value: '######'},
+  {key: 'p', value: ''}
+];
 
 const getHtmlByUrl = async (href) => {
   return new Promise((resolve, reject) => {
     request(href, (err, response, body) => {
       if (!err && response.statusCode == 200) {
         // console.log(body); // body即为目标页面的html
-        try {
-          await html2Md(body);
-        } catch(e) {
-          console.log(`html parse md err => ${e}`);
-        }
+        html2Md(body);
         resolve();
       } else {
         console.log('get page error url => ' + href);
@@ -20,14 +31,52 @@ const getHtmlByUrl = async (href) => {
       }
     });
   });
-}
+};
 
-const html2Md = async (str) => {
+const html2Xml = async (str) => {
   return new Promise((resolve, reject) => {
     let $ = cheerio.load(str);
     let contentStr = $('#main-content').html();
+    const doc = new xmlDom().parseFromString(contentStr);
+    // console.info(doc.childNodes.length);
+    const length = doc.childNodes.length;
+
+   for (let nodeIndex = 0; nodeIndex < length; nodeIndex ++) {
+    deepTraversal(doc.childNodes[nodeIndex]);
+   }
+   console.info(markdownStr);
+   return markdownStr;
+
   });
-}
+};
+
+const deepTraversal = (node) => {
+  let nodes = [];
+  if (node !== null) {
+    nodes.push(node);
+    let children = node.childNodes;
+    // console.info(children);
+    markdownStr += xml2Md(node);
+    if (children) {
+      for (let i = 0; i < children.length; i++ ) {
+        deepTraversal(children[i].childNodes);
+      }
+    }
+  }
+  // return nodes;
+};
+
+const xml2Md = (node) => {
+  let mds = ``;
+  tagHtml.filter((item, index) => {
+    if (item.key === node.nodeName) {
+      mds += `${item.value} ${node.childNodes[0].data} \n`;
+    }
+  });
+  return mds;
+};
+
+
 
 // module.exports = getHtmlByUrl('https://www.baidu.com/');
-module.exports = html2Md(demostr);
+module.exports = html2Xml(demostr);
