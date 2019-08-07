05498d2578caf8620d616211842d09cb58a3e4c1
聂玉玲 <nieyuling@nieyulingdeMacBook-Pro.local>
2019-08-07 09:20:24 +0800

    a转化

--- server/html2md.js
+++ server/html2md.js
@@ -16,6 +16,8 @@ const tagHtml = [
   {key: 'h5', value: '#####'},
   {key: 'h6', value: '######'},
   {key: 'p', value: ''},
+  {key: 'span', value: ''},
+  {key: 'div', value: ''},
   {key: 'a', value: '[]()'},
   {key: 'li', value: '-'},
   {key: 'img', value: '![]'}
@@ -41,11 +43,16 @@ const html2Xml = async (str) => {
     let $ = cheerio.load(str);
     let contentStr = $('#main-content').html();
     const doc = new xmlDom().parseFromString(contentStr);
-    const length = doc.childNodes.length;
+    let length = 0;
+    // if (doc && doc.childNodes && doc.childNodes.length > 0){
+    //   length = doc.childNodes.length;
+    // }
+    length = doc.childNodes && doc.childNodes.length ;
     // console.log(length);
 
-    for (let nodeIndex = 0; nodeIndex < 3; nodeIndex ++) {
+    for (let nodeIndex = 1; nodeIndex < 2; nodeIndex ++) {
       deepTraversal(doc.childNodes[nodeIndex]);
+      // console.info(doc.childNodes[nodeIndex]);
     }
    console.info(markdownStr);
    return markdownStr;
@@ -54,9 +61,9 @@ const html2Xml = async (str) => {
 };
 
 const deepTraversal = (node) => {
-  // console.log(`***********************start*********************\n`);
-  // console.info(node);
-  // console.log(`***********************end*********************\n`);
+  console.log(`***********************start*********************\n\n`);
+  console.info(node);
+  console.log(`***********************end*********************\n\n`);
   let nodes = [];
   if (node !== null) {
     nodes.push(node);
@@ -87,10 +94,18 @@ const xml2Md = (node) => {
         }
         mds += `${item.value}(${imgSrc})\n`;
       } else if (item.key === 'a') {
-        console.info(node.attributes);
-        // node.attributes.filter((aItem) => {
-
-        // });
+        // console.log(`***********************node.attributes start*********************\n\n`);
+        // console.info(node.attributes);
+        // console.log(`***********************node.attributes end*********************\n\n`);
+        let alink = '', ades = '';
+        for (let aIndex = 0; aIndex < node.attributes.length; aIndex ++) {
+            const aItem = node.attributes[aIndex];
+            if (aItem.nodeName === 'href') {
+              alink = aItem.nodeValue;
+            } 
+        }
+        alink = `[${node.childNodes[0],data}](#${alink})`;
+        mds += `${alink} \n`;
       } else {
         mds += `${item.value} ${node.childNodes[0].data || node.data} \n`;
       }
