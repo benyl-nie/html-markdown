02f5f0567565af30ecd472854a1e8d10394a9d1b
nieyuling02 <nieyuling02@maoyan.com>
2019-08-12 17:52:35 +0800

    问题修复

--- server/html2md.js
+++ server/html2md.js
@@ -1,6 +1,12 @@
+/**
+ * confluence 转 markdown
+ * @param: {url, cookie}
+ * @output markdownStr
+ * url -> 截取某个id内url -> 转成xml -> 循环遍历xml节点信息,边遍历边将指定节点信息转成markdown格式,遍历后至节点hasRead为true -> 输出markdownStr
+ */
+
 const request = require('request');
 const cheerio = require('cheerio');
-// const lodash = require('lodash');
 const xpath = require('xpath');
 const xmlDom = require('xmldom').DOMParser;
 
@@ -8,6 +14,9 @@ const demostr = require('./tpl/ftl');
 const httpUrl = 'https://wiki.maoyan.com';
 
 let markdownStr = ``;
+
+const IMGSRC = 'data-image-src';
+const BASEURL = 'data-base-url';
 
 
 const tagHtml = [
@@ -21,24 +30,24 @@ const tagHtml = [
   {key: 'hr', value: '---'},
   {key: 'p', value: ''},
   {key: 'span', value: ''},
+  {key: 'pre', value: ''},
   {key: 'br', value: ''},
-  // {key: 'div', value: ''},
-  // {key: 'table', value: ''},
   {key: 'thead', value: ''},
   {key: 'tbody', value: ''},
-  // {key: 'tr', value: ''},
-  // {key: 'th', value: ''},
-  // {key: 'td', value: ''},
   {key: 'a', value: '[]()'},
   // {key: 'ul', value: ''},
   {key: 'li', value: '*'},
-  {key: 'img', value: '![]'}
+  {key: 'img', value: '![]'},
+  {key: 'br', value: `\n`}
 ];
 
 const blockLevelTag = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+const textLevelTag = ['span', '#text', 'br', 'pre', 'a', 'b', 'label', 'strong'];
+const tableNode = ['tr', 'td', 'th'];
+const lineFlagArr = ['#', '*', '', '|'];
 
 
-
+// 根据url爬取页面
 const getHtmlByUrl = async (href) => {
   return new Promise((resolve, reject) => {
     request(href, (err, response, body) => {
@@ -61,23 +70,24 @@ const html2Xml = async (str) => {
     const doc = new xmlDom().parseFromString(contentStr);
     let length = 0;
     length = doc.childNodes && doc.childNodes.length ;
-    // console.log(length);
     for (let nodeIndex = 0; nodeIndex < length; nodeIndex ++) {
       deepTraversal(doc.childNodes[nodeIndex]);
     }
-  //  console.info(markdownStr);
+   console.info(markdownStr);
    return markdownStr;
-
   });
 };
 
 const deepTraversal = (node) => {
   let nodes = [];
 
-  // 过滤目录
+  /**
+   *  过滤目录
+   *  过滤code中toolbar div
+   * */
   const marcDom = new xmlDom().parseFromString(node.toString());
   const marcNode = xpath.select1("/div/@class", marcDom);
-  if (marcNode !== undefined && marcNode.value.indexOf('toc') > -1) {
+  if ((marcNode !== undefined && marcNode.value.indexOf('toc') > -1) || (marcNode !== undefined && marcNode.value === 'toolbar')) {
     return;
   }
 
@@ -85,7 +95,13 @@ const deepTraversal = (node) => {
     nodes.push(node);
     let children = node.childNodes;
     const markdown = xml2Md(node);
-    markdownStr +=  (markdown.trim().substr(-1) === '#' || markdown.trim().substr(-1) === '*' || markdown === '' || markdown.trim().substr(-1) === '|') ? `${markdown}` : `${markdown}\n` ;
+    // textLevelTag.indexOf(node.nodeName) > -1 ||
+    if (lineFlagArr.indexOf(markdown.trim().substr(-1)) > -1) {
+      markdownStr += `${markdown}`;
+    } else {
+      markdownStr += `${markdown}\n`;
+    }
+    // markdownStr +=  (markdown.trim().substr(-1) === '#' || markdown.trim().substr(-1) === '*' || markdown === '' || markdown.trim().substr(-1) === '|') ? `${markdown}` : `${markdown}\n` ;
     // markdownStr = markdownStr.replace(`\n\n`, '');
     if (children) {
       for (let i = 0; i < children.length; i++ ) {
@@ -98,85 +114,132 @@ const deepTraversal = (node) => {
 
 const xml2Md = (node) => {
   let mds = ``;
-  
+
   tagHtml.filter((item) => {
     const nodeName = node.nodeName;
     if (item.key === nodeName) {
-      if (item.key === 'img') {
-        let attrLen = node.attributes.length;
-        let imgBase = '', imgSour = '', imgSrc = '';
-        for (let attrIndex = 0; attrIndex < attrLen; attrIndex ++) {
-          const attrItem = node.attributes[attrIndex];
-          if (attrItem.nodeName === 'data-image-src') imgSour = attrItem.nodeValue;
-          if (attrItem.nodeName === 'data-base-url') imgBase = attrItem.nodeValue;
-          imgSrc = imgBase + imgSour;
-        }
-        mds += `${item.value}(${imgSrc})`;
-      } else if (item.key === 'a') {
-        let alink = '';
-        for (let aIndex = 0; aIndex < node.attributes.length; aIndex ++) {
-            const aItem = node.attributes[aIndex];
-            if (aItem.nodeName === 'href') {
-              alink = aItem.nodeValue;
-            }
-        }
-        if (alink.substr(0,1) === '/') alink = `${httpUrl}${alink}`;
-        alink = `[${node.childNodes[0].data}](${alink})`;
-        mds += `${alink}`;
-      } else if (item.key === 'thead') {
-        let theadmd = ``;
-        theadmd = table2Md(node, 'thead');
-        mds += theadmd;
-      } else if (item.key === 'tbody') {
-        let tbodymd = ``;
-        // console.log('***********start*************');
-        // console.info(node.parentNode.nodeName);
-        // console.log('***********end*************');
-        tbodymd = table2Md(node, 'tbody');
-        mds += tbodymd;
-      } else if (item.key === 'strong') {
-        const value = (node.childNodes && node.childNodes[0] && node.childNodes[0].data) || (node && node.data) || '';
-        mds += value === '' ? '' : `**${value}**`;
-        for (let boldAttrIndex = 0; boldAttrIndex < node.childNodes.length; boldAttrIndex ++) {
-          // console.info(node.childNodes[boldAttrIndex]);
-          // mds += xml2Md(node.childNodes[boldAttrIndex]);
-        }
-      } else if (item.key === 'p') {
-        const pDom = new xmlDom().parseFromString(node.toString());
-        const pNode = xpath.select("//p/text()", pDom);
-        let md = ``;
-        if (pNode.length> 0) {
-          for (let pIndex = 0; pIndex < pNode.length; pIndex ++) {
-            md += pNode[pIndex].nodeValue;
-          }
-
-        }
-        mds += md;
-
-      } else {
-          const value = (node.childNodes && node.childNodes[0] && node.childNodes[0].data) || (node && node.data) || '';
-          let md = value !== '' ? `${item.value} ${value}` : `${item.value} `;
-        mds += md;
-
+      let md = ``;
+      switch(item.key) {
+        case 'img':
+          md = img2Md(node, item);
+          break;
+        case 'a':
+          md = a2Md(node, item);
+          break;
+        case 'thead':
+          md = table2Md(node, 'thead');
+          break;
+        case 'tbody':
+          md = table2Md(node, 'tbody') + `\n`;
+          break;
+        case 'strong':
+          md = strong2Md(node, item);
+          break;
+        case 'p':
+          md = p2Md(node, item);
+          break;
+        default:
+          md = default2Md(node, item);
+          break;
       }
+      mds += md;
     }
   });
   mds.replace(`\n`, '');
   return mds;
 };
 
+const default2Md = (node, item) => {
+  /**
+   * 过滤由table带过来的遍历
+   */
+  const parentDom = new xmlDom().parseFromString(node.parentNode.toString());
+  const parentClass = xpath.select1("//@class", parentDom) && xpath.select1("//@class", parentDom).nodeValue;
+  if (parentClass && parentClass.indexOf('table') > -1) return '';
+  if (tableNode.indexOf(node.parentNode.nodeName) > -1) {
+    return '';
+  }
+  if (node.hasRead) return '';
+  const value = (node.childNodes && node.childNodes[0] && node.childNodes[0].data) || (node && node.data) || '';
+  const md = value !== '' ? `${item.value} ${value}` : `${item.value} `;
+  node.hasRead = true;
+  return md;
+}
+
+const img2Md = (node, item) => {
+  let attrLen = node.attributes.length;
+  let imgBase = '', imgSour = '', imgSrc = '';
+  for (let attrIndex = 0; attrIndex < attrLen; attrIndex ++) {
+    const attrItem = node.attributes[attrIndex];
+    if (attrItem.nodeName === IMGSRC) imgSour = attrItem.nodeValue;
+    if (attrItem.nodeName === BASEURL) imgBase = attrItem.nodeValue;
+    imgSrc = imgBase + imgSour;
+  }
+  node.hasRead = true;
+  return `${item.value}(${imgSrc})`;
+}
+
+const a2Md = (node, item) => {
+  let alink = '';
+  if (node.hasRead) return '';
+  for (let aIndex = 0; aIndex < node.attributes.length; aIndex ++) {
+      const aItem = node.attributes[aIndex];
+      if (aItem.nodeName === 'href') {
+        alink = aItem.nodeValue;
+      }
+  }
+  if (alink.substr(0,1) === '/') alink = `${httpUrl}${alink}`;
+  alink = `[${node.childNodes[0].data}](${alink})`;
+  node.hasRead = true;
+  return`${alink}`;
+}
+
+const strong2Md = (node, item) => {
+  let md = ``;
+  const value = (node.childNodes && node.childNodes[0] && node.childNodes[0].data) || (node && node.data) || '';
+  for (let boldAttrIndex = 0; boldAttrIndex < node.childNodes.length; boldAttrIndex ++) {
+    // console.info(node.childNodes[boldAttrIndex]);
+    // mds += xml2Md(node.childNodes[boldAttrIndex]);
+  }
+  md += value === '' ? '' : `**${value}**`;
+  node.hasRead = true;
+  return md;
+}
+
+const p2Md = (node, item) => {
+  const nodeLen = node.childNodes.length;
+  let md = ``;
+  for (let nodeIndex = 0; nodeIndex < nodeLen; nodeIndex ++) {
+    const flag = tagHtml.filter(item => item.key === node.childNodes[nodeIndex].nodeName);
+    if (flag.length > 0) {
+      md += xml2Md(node.childNodes[nodeIndex]);
+    } else {
+      const pDom = new xmlDom().parseFromString(node.childNodes[nodeIndex].toString());
+      const pNode = xpath.select("//text()", pDom);
+      for (let pIndex = 0; pIndex < pNode.length; pIndex ++) {
+        md += pNode[pIndex].nodeValue;
+      }
+    }
+    node.childNodes[nodeIndex].hasRead = true;
+  }
+  return md;
+}
+
 const table2Md = (node, type) => {
   const theadAttr = node.attributes;
   if (type === 'tbody') {
     let tbodymd = tbody2Md(node) || ``;
+    node.hasRead = true;
     return tbodymd;
   } else {
     for (let attrIndex = 0; attrIndex < theadAttr.length; attrIndex ++) {
       if (theadAttr[attrIndex].nodeName === 'class' && theadAttr[attrIndex].nodeValue === 'tableFloatingHeaderOriginal') {
         let theadmd = thead2Md(node) || ``;
+        node.hasRead = true;
         return theadmd;
       } else {
         let theadmd = '';
+        node.hasRead = true;
         return theadmd;
       }
     }
@@ -185,14 +248,22 @@ const table2Md = (node, type) => {
 
 const thead2Md = (node) => {
   const dom = new xmlDom().parseFromString(node.toString());
-  const divxpath = xpath.select("//div", dom);
-  let theadmd = ``;
+  const divxpath = xpath.select("//tr", dom);
+  let theadmd = ``, thdom = null, tdpath = null;
   for(let t = 0; t < divxpath.length; t++) {
-    theadmd += `${divxpath[t].childNodes[0].data} |`
+    thdom = new xmlDom().parseFromString(divxpath[t].toString());
+    tdpath = xpath.select("//th", thdom)
+    for(let thIndex = 0; thIndex < tdpath.length; thIndex ++) {
+      let tddom = new xmlDom().parseFromString(tdpath[thIndex].toString());
+      const tdtext = xpath.select("//text()", tddom);
+      theadmd += `${tdtext[0].nodeValue} |`
+    }
+    theadmd += `\n`;
   }
-  theadmd += `\n`;
   for(let t = 0; t < divxpath.length; t++) {
-    theadmd += `------------ |`
+    for(let thIndex = 0; thIndex < tdpath.length; thIndex ++) {
+      theadmd += `------------ |`
+    }
   }
   theadmd += `\n`
   return theadmd;
@@ -202,12 +273,11 @@ const tbody2Md = (node) => {
   const trDom = new xmlDom().parseFromString(node.toString());
   const trxpath = xpath.select('//tr', trDom);
   let tbodymd = ``;
-  
+
   for (let trIndex = 0; trIndex < trxpath.length; trIndex ++) {
     if (trxpath[trIndex].toString().indexOf('code') > -1) {
       // code转化
       tbodymd += codeTrMd(trxpath[trIndex]);
-      console.info(tbodymd);
     } else {
       tbodymd += tr2Md(trxpath[trIndex]);
     }
@@ -220,37 +290,45 @@ const tr2Md = (node) => {
   const tdpath = xpath.select("//td", tdDom);
   let trmd = ``;
   for (let tdIndex = 0; tdIndex < tdpath.length; tdIndex ++) {
-    
+
     let tdmd = ``;
-    for (let tdChildIndex = 0; tdChildIndex < tdpath[tdIndex].childNodes.length; tdChildIndex ++ ) {
-      if (tdpath[tdIndex].childNodes[tdChildIndex].nodeName === '#text') {
-        tdmd += tdpath[tdIndex].childNodes[tdChildIndex].data + '|';
-      } else {
-        // console.log('***********start*************');
-        // console.info( tdpath[tdIndex]);
-        // console.log('***********end*************');
-        tdmd += xml2Md(tdpath[tdIndex].childNodes[tdChildIndex]) + '|';
-      }
-    }
+    tdmd = `${td2Md(tdpath[tdIndex])} |`;
     trmd += tdmd ;
   }
   trmd += `\n`;
   return trmd;
 }
 
+td2Md = (node) => {
+  const domtd = new xmlDom().parseFromString(node.toString());
+  const domtdVal = xpath.select("//text()", domtd);
+  let tdmd = ``;
+  for(let tdmdIndex = 0; tdmdIndex < domtdVal.length; tdmdIndex ++) {
+    tdmd += `${domtdVal[tdmdIndex].data}  `;
+  }
+  return tdmd;
+}
+
+// code to markdown
 const codeTrMd = (node) => {
   const tdDom = new xmlDom().parseFromString(node.toString());
   const tdpath = xpath.select("//td/div/div", tdDom);
-  let codemd = '' + '```' + `\n`;
+  let codemd = '\n' + '```' + `\n`;
   for(let codeIndex = 0; codeIndex < tdpath.length; codeIndex ++) {
     const codeDom = new xmlDom().parseFromString(tdpath[codeIndex].toString());
     let mdValue = xpath.select("//code/text()", codeDom);
-    console.info(mdValue[0].data);
-    console.log('***********end*************');
-    // codemd += mdValue + `\n`;
+
+    for (let mdValueIndex = 0; mdValueIndex < mdValue.length; mdValueIndex ++) {
+      codemd += mdValue[mdValueIndex].data;
+    }
+    codemd += `\n`;
   }
   codemd += '```\n';
   return codemd;
+}
+
+const htmlEncode = (str) => {
+  return str.replace(/[<>"&\/`']/g, '');
 }
 
 // module.exports = getHtmlByUrl('https://www.baidu.com/');
